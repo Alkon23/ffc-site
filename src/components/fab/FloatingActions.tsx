04198e57670308392096ce1039ec8f15e772768b
alkon23 <belatz.arcega@gmail.com>
2024-03-09 15:37:05 +0100

    Visual improvements

--- src/components/fab/FloatingActions.tsx
+++ src/components/fab/FloatingActions.tsx
@@ -4,7 +4,7 @@ import WhatsAppButton from "./WhatsAppButton.tsx";
 export default function FloatingActions() {
 
   return (
-    <div className={"fixed bottom-5 right-5 z-40"}>
+    <div className="fixed bottom-5 right-5 z-40 drop-shadow-[0_2px_3px_rgba(0,0,0,0.3)]">
       <ScrollTopButton/>
       <WhatsAppButton/>
     </div>
--- src/components/shared/HeaderSmall.tsx
+++ src/components/shared/HeaderSmall.tsx
@@ -5,7 +5,7 @@ export default function HeaderSmall(props: HeaderProps) {
   return (
     <header>
         <div className="bg-concrete bg-cover bg-no-repeat bg-center h-[calc(100vh/2.2)] relative">
-          <div className="w-full h-full absolute top-0 left-0 flex justify-center items-center">
+          <div className="w-full h-full pt-6 absolute top-0 left-0 flex justify-center items-center">
             <p className="font-anton tracking-wide uppercase select-none text-8xl text-white relative w-full z-0 ml-40 mt-5">
               {props.title}
               <span className="text-[6.55rem] font-extrabold text-stroke-white opacity-90 absolute -top-14 left-16 -z-10 scale-y-[1.3]">
