91e5f9e2daba43d4fbee571d9334aefa22c194cd
alkon23 <belatz.arcega@gmail.com>
2024-03-29 12:48:58 +0100

    Lightly change ViewToggleButton animation

--- src/components/shared/ViewToggleButton.tsx
+++ src/components/shared/ViewToggleButton.tsx
@@ -10,9 +10,10 @@ export function ViewToggleButton(props: { onClick: () => void }) {
 
   return (
     <button onClick={handleClick} className={`relative border-0 p-0 h-[25px] w-[25px]`} type="button">
-      <span className={"block rounded-[2px] h-[2px] w-[25px] bg-black transition-all duration-200 ease-in-out translate-y-1/2 " +
+      <span className={"block rounded-[2px] h-[2px] w-[25px] bg-black transition-all duration-300 ease-in-out translate-y-1/2 " +
           `${isOpen ? "rotate-0" : "-rotate-90"}`}/>
-      <span className="block rounded-[2px] h-[2px] w-[25px] bg-black -translate-y-1/2"/>
+      <span className={"block rounded-[2px] h-[2px] w-[25px] bg-black transition-all duration-300 ease-in-out -translate-y-1/2 " +
+          `${isOpen ? "-rotate-180" : "rotate-0"}`}/>
     </button>
   )
 
